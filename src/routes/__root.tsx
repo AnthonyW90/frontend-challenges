1774081d4058608202e72201be5021809ffa0a81
Anthony Wallace <anthonyt.wallace@icloud.com>
2024-05-23 14:03:10 -0700

    Added footer component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Footer from "../components/footer";
 
 export const Route = createRootRoute({
   component: () => (
@@ -17,7 +18,8 @@ export const Route = createRootRoute({
       </div>
       <hr />
       <Outlet />
-      <TanStackRouterDevtools />
+      {/* <TanStackRouterDevtools /> */}
+      <Footer />
     </>
   ),
 });
